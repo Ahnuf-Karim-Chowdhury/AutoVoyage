d753afb4f92da28e27de8be6e62da92df1d94672
nahid <naas50dx@gmail.com>
2024-06-09 18:35:31 +0600

    Updated AutoVoyage2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Updated import
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/01HomePage/HomePage';
 import Login from './components/02Login/Login';
-import Signup from './components/03SignUp/Signup'
+import Signup from './components/03SignUp/Signup';
 import ForgotPassword from './components/04FogotPassword/ForgotPassword';
-
+import Deals from './components/New/Deals';
+import CompareCars from './components/Research/CompareCars';
+import CarReviews from './components/Research/CarReviews';
 
 function App() {
     return (
         <Router>
             <div>
                 <Navbar />
-                <Routes> {/* Use Routes instead of Switch */}
-                    <Route path="/" element={<HomePage />} /> {/* Updated Route */}
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/forgotpassword" element={<ForgotPassword />} />
-                    
-
+                    <Route path="/deals" element={<Deals />} />
+                    <Route path="/compare-cars" element={<CompareCars />} />
+                    <Route path="/car-reviews" element={<CarReviews />} />
                 </Routes>
             </div>
         </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,58 @@
-
 import { Link } from 'react-router-dom'; // Import Link
 import "./Navbar.css"
-const navbar = () => {
+
+const Navbar = () => {
   return (
     <nav className='nav'>
       <Link to="/" className="logo">
         AutoVoyage
       </Link>
+      
       <ul className="nav-links">
         <li className="dropdown">
           <a href="#" className="dropbtn">New</a>
           <div className="dropdown-content">
-            <a href="Nav-Bar Categories\01.New\01.Deals\13deals.html">Deals</a>
-            <a href="Nav-Bar Categories\01.New\02.New Car Listings\16new-car-listings.html">New Car Listings</a>
-            <a href="#">New SUVs</a>
-            <a href="#">New Trucks</a>
-            <a href="#">New Vans</a>
-            <a href="#">New Convertibles</a>
+            <Link to="/deals">Deals</Link>
+            <Link to="/new-car-listings">New Car Listings</Link>
+            <Link to="/new-suvs">New SUVs</Link>
+            <Link to="/new-trucks">New Trucks</Link>
+            <Link to="/new-vans">New Vans</Link>
+            <Link to="/new-convertibles">New Convertibles</Link>
           </div>
         </li>
         <li className="dropdown">
           <a href="#" className="dropbtn">Used</a>
           <div className="dropdown-content">
-            <a href="#">Used Car Listings</a>
-            <a href="#">Used EVs</a>
-            <a href="#">Used SUVs</a>
-            <a href="#">Used Trucks</a>
-            <a href="#">Used Vans</a>
-            <a href="#">Used Convertibles</a>
+            <Link to="/used-car-listings">Used Car Listings</Link>
+            <Link to="/used-evs">Used EVs</Link>
+            <Link to="/used-suvs">Used SUVs</Link>
+            <Link to="/used-trucks">Used Trucks</Link>
+            <Link to="/used-vans">Used Vans</Link>
+            <Link to="/used-convertibles">Used Convertibles</Link>
           </div>
         </li>
         <li className="dropdown">
           <a href="#" className="dropbtn">Electric</a>
           <div className="dropdown-content">
-            <a href="#">EV Hub</a>
-            <a href="#">New EVs</a>
-            <a href="#">EV Incentives</a>
-            <a href="#">EV Deals</a>
-            <a href="#">Buyers Guide</a>
+            <Link to="/ev-hub">EV Hub</Link>
+            <Link to="/new-evs">New EVs</Link>
+            <Link to="/ev-incentives">EV Incentives</Link>
+            <Link to="/ev-deals">EV Deals</Link>
+            <Link to="/buyers-guide">Buyers Guide</Link>
           </div>
         </li>
         <li className="dropdown">
           <a href="#" className="dropbtn">Research</a>
           <div className="dropdown-content">
-            <a href="#">Car Reviews</a>
-            <a href="#">Car Rankings</a>
-            <a href="#">Compare Cars</a>
-            <a href="#">Car Finder Quiz</a>
-            <a href="#">Lease a Car</a>
-            <a href="#">Electric Vehicles</a>
-            <a href="#">Research Center</a>
+            <Link to="/car-reviews">Car Reviews</Link>
+            <Link to="/car-rankings">Car Rankings</Link>
+            <Link to="/compare-cars">Compare Cars</Link>
+            <Link to="/car-finder-quiz">Car Finder Quiz</Link>
           </div>
         </li>
-        <li><a href="#">Sell Your Car</a></li>
+        <li><Link to="/sell-your-car">Sell Your Car</Link></li>
       </ul>
+      
       <div className="search-box">
         <input type="text" id="search-input" placeholder="Search cars" />
         <i className="fas fa-search"></i>
@@ -65,11 +64,10 @@ const navbar = () => {
         <Link to="/signup">Sign Up</Link>
       </div>
       <div className="profile-icon">
-        <a href="10profile.html"><i className="fas fa-user-circle"></i></a>
+        <Link to="/profile"><i className="fas fa-user-circle"></i></Link>
       </div>
     </nav>
-
   );
 };
 
-export default navbar;
+export default Navbar;
