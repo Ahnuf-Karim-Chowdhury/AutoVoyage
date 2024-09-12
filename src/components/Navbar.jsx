e2fc30cd74e6dabc75b84d5f0e42527a241708f0
nahid <naas50dx@gmail.com>
2024-09-12 13:13:27 +0600

    Updated AutoVoyage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../backend/AuthStuff/AuthContext.jsx'; // Import useAuth
 import './Navbar.css';
@@ -6,12 +5,6 @@ import './Navbar.css';
 const Navbar = () => {
   const { isAuthenticated, logout } = useAuth();
   
-=======
-import { Link } from 'react-router-dom'; // Import Link
-import "./Navbar.css"
-
-const Navbar = () => {
->>>>>>> 2aa5575cbc578acdd56b3ec28a41a4dc564fdb1f
   return (
     <nav className='nav'>
       <Link to="/" className="logo">
@@ -67,7 +60,6 @@ const Navbar = () => {
       </div>
 
       <div className="auth-links">
-<<<<<<< HEAD
         {isAuthenticated ? (
           <>
             <ul className="nav-links">
@@ -89,13 +81,6 @@ const Navbar = () => {
             <Link to="/signup">Sign Up</Link>
           </>
         )}
-=======
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign Up</Link>
-      </div>
-      <div className="profile-icon">
-        <Link to="/profile"><i className="fas fa-user-circle"></i></Link>
->>>>>>> 2aa5575cbc578acdd56b3ec28a41a4dc564fdb1f
       </div>
     </nav>
   );
